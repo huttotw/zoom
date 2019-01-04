2f63a94388ade81d7aad9d1251fc2071aa3e6299
Trevor Hutto <huttotw@gmail.com>
2019-01-04 16:57:59 -0500

    trash the response

--- cmd/zoom/do.go
+++ cmd/zoom/do.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"io"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
 
 func do(s *stats, reqs <-chan *http.Request, errs chan error) {
-	c := http.Client{}
+	c := http.Client{
+		Transport: &http.Transport{
+			MaxIdleConns: 0,
+			MaxIdleConnsPerHost: 100,
+			MaxConnsPerHost: 0,
+		},
+	}
 	for req := range reqs {
 		start := time.Now()
 		resp, err := c.Do(req)
@@ -14,6 +22,7 @@ func do(s *stats, reqs <-chan *http.Request, errs chan error) {
 			errs <- err
 			continue
 		}
+		io.Copy(ioutil.Discard, resp.Body)
 		resp.Body.Close()
 		s.incrementRequests(1)
 		s.recordStatus(resp.StatusCode)
